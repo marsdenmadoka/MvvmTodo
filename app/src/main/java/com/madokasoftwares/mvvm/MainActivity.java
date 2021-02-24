37090570279c12f3678b40e3cdaab72ebaee5f9e
marsdenmadoka <marsmadoka98@gmail.com>
2021-02-24 23:30:59 +0300

    added our adapter

--- app/src/main/java/com/madokasoftwares/mvvm/MainActivity.java
+++ app/src/main/java/com/madokasoftwares/mvvm/MainActivity.java
@@ -3,11 +3,15 @@ package com.madokasoftwares.mvvm;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 import android.os.Bundle;
+import android.widget.Adapter;
 import android.widget.Toast;
 
+import com.madokasoftwares.mvvm.Adapter.NoteAdapter;
 import com.madokasoftwares.mvvm.Room.Note;
 import com.madokasoftwares.mvvm.ViewModel.NoteViewModel;
 
@@ -20,6 +24,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+      RecyclerView recyclerView = findViewById(R.id.recycler_view);
+      recyclerView.setLayoutManager(new LinearLayoutManager(this));
+      recyclerView.setHasFixedSize(true);
+
+      NoteAdapter adapter = new NoteAdapter();
+      recyclerView.setAdapter(adapter);
+
 
         noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);//the android system will destroy the model when the activity is finished
         //this returns livedata
@@ -27,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
            @Override
            public void onChanged(List<Note> notes) {
          //update our reycleview
-               Toast.makeText(MainActivity.this,"welcome home",Toast.LENGTH_SHORT).show();
+               adapter.setNotes(notes);//set notes was our update method we created
 
            }
        });
