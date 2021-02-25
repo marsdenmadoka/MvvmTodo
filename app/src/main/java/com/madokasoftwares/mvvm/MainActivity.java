e273383d6ddac49f8e33b23780168e88e35ce829
marsdenmadoka <marsmadoka98@gmail.com>
2021-02-25 13:48:01 +0300

    added our addNote activity

--- app/src/main/java/com/madokasoftwares/mvvm/MainActivity.java
+++ app/src/main/java/com/madokasoftwares/mvvm/MainActivity.java
@@ -1,5 +1,6 @@
 package com.madokasoftwares.mvvm;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -7,10 +8,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Adapter;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.madokasoftwares.mvvm.Adapter.NoteAdapter;
 import com.madokasoftwares.mvvm.Room.Note;
 import com.madokasoftwares.mvvm.ViewModel.NoteViewModel;
@@ -18,12 +22,26 @@ import com.madokasoftwares.mvvm.ViewModel.NoteViewModel;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int ADD_NOTE_REQUEST=1;
 
     private NoteViewModel noteViewModel; //member variable for our viewmodel
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton buttonAddNote = findViewById(R.id.button_add_note);
+        buttonAddNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,AddNoteActivity.class);
+                startActivityForResult(intent,ADD_NOTE_REQUEST); //this is because we want to get the results/input from addnoteactivity back
+                //ADD_NOTE_REQUEST s this will help us to distuguish between the requests since we can call start activity for different activity
+            }
+        });
+
+
+
       RecyclerView recyclerView = findViewById(R.id.recycler_view);
       recyclerView.setLayoutManager(new LinearLayoutManager(this));
       recyclerView.setHasFixedSize(true);
@@ -43,5 +61,24 @@ public class MainActivity extends AppCompatActivity {
            }
        });
 
+    }
+    //getting the results from AddNoteActivity
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == ADD_NOTE_REQUEST && requestCode == RESULT_OK){ //in order t receive te intents well our resultscode should match
+          //we now retrive the extras
+          String title = data.getStringExtra(AddNoteActivity.EXTRA_TITLE);
+          String description = data.getStringExtra(AddNoteActivity.EXTRA_DESCRIPTION);
+          int priority = data.getIntExtra(AddNoteActivity.EXTRA_PRIORITY,1);//we use 1 so that incase the int is missing we assign the 1
+
+            //after we get the results in our main activity we want to save them to the database
+            Note note = new Note(title,description,priority);
+            noteViewModel.insert(note);
+            Toast.makeText(this,"Note Saved!",Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this,"Note failed  Saved!",Toast.LENGTH_SHORT).show();
+        }
     }
 }
